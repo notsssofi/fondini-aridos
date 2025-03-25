308ec21d394a630347cc02d494c33443960ebba7
notsssofi <145951760+notsssofi@users.noreply.github.com>
2025-03-25 17:36:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { getDatabase, ref, set, get, push, update, remove } from "firebase/database";
-
 // Configuración de Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBw2XLVQTZE5XJAusfiZ16HjoTJJkNZLvg",
